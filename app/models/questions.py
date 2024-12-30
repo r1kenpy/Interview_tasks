3abed26082b81d5a0af9c958055cfd39c94a0153
Vladimir Molchanov <r1ken@mail.ru>
2024-12-30 18:12:33 +0300

    Добавлены модели и настроен алембик

---
+++ app/models/questions.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+from sqlalchemy import Integer, String, Text, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.core.db import Base
+from app.models.answers import Answer
+from app.models.blocks import Block
+
+
+class Question(Base):
+    title: Mapped[str] = mapped_column(String(256))
+    additional = Mapped[str] = mapped_column(Text, nullable=True)
+
+    answer_id: Mapped[int] = mapped_column(ForeignKey('answer.id'))
+    answer: Mapped[list['Answer']] = relationship(back_populates='question')
+
+    block_id: Mapped[int] = mapped_column(ForeignKey('block.id'))
+    block: Mapped[list['Block']] = relationship(back_populates='question')
+    created_at: Mapped[datetime] = mapped_column(
+        datetime,
+        default=datetime.now,
+        server_default=datetime.now,
+    )
+    interview_count: Mapped[int] = mapped_column(
+        Integer,
+        default=0,
+        server_default=0,
+    )
+    time_decision: Mapped[int] = mapped_column(Integer)
